5078c4b1f2d4590e74b24de2ee40f3d003300195
Gideon Goddard <goddard.gcw@gmail.com>
2019-12-03 14:36:48 +0000

    Completed classes without using the step-by-step instructions.

---
+++ app.js
@@ -0,0 +1,93 @@
+// MEDIA CLASS
+class Media {
+    constructor(title) {
+      this._title = title;
+      this._isCheckedOut = false;
+      this._ratings = [];
+    }
+    
+    get title() {
+      return this._title;
+    }
+    
+    get isCheckedOut() {
+      return this._isCheckedOut;
+    }
+    
+    get ratings() {
+      return this._ratings;
+    }
+    
+    getAverageRating() {
+      let sumRating =0;
+      for (let i = 0; i < this._ratings.length; i++) {
+        sumRating += this._ratings[i];
+      }
+      let averageRating = Math.round((sumRating / this._ratings.length) * 10) / 10;
+      return averageRating;
+    }
+    
+    toggleCheckOutStatus() {
+      if (this._isCheckedOut) {
+        this._isCheckedOut = false;
+      } else {
+        this._isCheckedOut = true;
+      }
+    }
+    
+    addRating(newRating) {
+      this._ratings.push(newRating);
+    }
+  }
+  
+  // BOOK CLASS
+  class Book extends Media {
+    constructor(author, title, pages) {
+      super(title);
+      this._author = author;
+      this._pages = pages;
+    }
+    
+    get author() {
+      return this._author;
+    }
+    
+    get pages() {
+      return this._pages;
+    }
+  }
+  
+  // MOVIE CLASS
+  class Movie extends Media {
+    constructor(director, title, runTime) {
+      super(title);
+      this._director = director;
+      this._runTime = runTime;
+    }
+    
+    get director() {
+      return this._director;
+    }
+    
+    get runTime() {
+      return this._runTime;
+    }
+  }
+  
+  // CD CLASS
+  class CD extends Media {
+    constructor(artist, title, songs) {
+      super(title);
+      this._artist = artist;
+      this._songs = songs;
+    }
+    
+    get artist() {
+      return this._artist;
+    }
+    
+    get songs() {
+      return this._songs;
+    }
+  }
+  
